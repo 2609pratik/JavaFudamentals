52dda88aea23357dc367a7347ddda42861056606
2609pratik <pratikbhagadkar2609@gmail.com>
2022-08-08 00:31:43 +0530

    conflict

--- src/startOfJava/HelloWorld.java
+++ src/startOfJava/HelloWorld.java
@@ -2,7 +2,7 @@ package startOfJava;
 
 public class HelloWorld {
 	public static void main(String args[]) {
-		System.out.println("hello from local side.....!!!");
+		System.out.println("hello from local side....!!!");
 		System.out.println("Hello World ....!!!");
 		
 	}	
